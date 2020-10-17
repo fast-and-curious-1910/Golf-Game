e6b4c12e5ce0e7067bd1f8e637bf927844a3e477
Daniel <71951166+Myphz@users.noreply.github.com>
2020-10-17 12:32:50 +0200

    Update startScreen.py

--- startScreen.py
+++ startScreen.py
@@ -7,9 +7,9 @@ import sys
 pygame.init()
 
 win = pygame.display.set_mode((1080, 600))
-title = pygame.image.load(os.path.join('img', 'title.png'))
-back = pygame.image.load(os.path.join('img', 'back.png'))
-course = pygame.image.load(os.path.join('img', 'course1.png'))
+title = pygame.image.load(os.path.join('imgs', 'title.png'))
+back = pygame.image.load(os.path.join('imgs', 'back.png'))
+course = pygame.image.load(os.path.join('imgs', 'course1.png'))
 course1 = pygame.transform.scale(course, (200, 200))
 
 font = pygame.font.SysFont('comicsansms', 24)
